24fafe7d1da645192f0a4acdd0283b1de2e78df3
beedell.rokejulianlockhart <rokejulianlockhart+1674177493@outlook.com>
2023-02-10 00:30:31 +0000

    Update (Duplicate-Repository).bin.txt.ps1
    Signed-off-by: beedell.rokejulianlockhart <rokejulianlockhart+1674177493@outlook.com>

--- (Duplicate-Repository).bin.txt.ps1
+++ (Duplicate-Repository).bin.txt.ps1
@@ -2,5 +2,5 @@
 
 Set-Variable -Name Username -Scope Global -Value rokejulianlockhart
 git clone --bare https://github.com/rokejulianlockhart/$(Get-Variable -Name 'Username' | Select-Object -ExpandProperty Value).git
-Set-Location (Get-Variable -Name 'PID' | Select-Object -ExpandProperty Value)
+Set-Location (Get-Variable -Name 'Username' | Select-Object -ExpandProperty Value)
 git push --mirror "https://github.com/rokejulianlockhart/$(Get-Variable -Name 'Username' | Select-Object -ExpandProperty Value).github.io.git"
