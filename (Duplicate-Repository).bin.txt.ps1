e8560bb081a8f9319f490bee5b60c53b07dd39f4
beedell.rokejulianlockhart <rokejulianlockhart+1674177493@outlook.com>
2023-02-10 01:43:22 +0000

    Update (Duplicate-Repository).bin.txt.ps1
    Signed-off-by: beedell.rokejulianlockhart <rokejulianlockhart+1674177493@outlook.com>

--- (Duplicate-Repository).bin.txt.ps1
+++ (Duplicate-Repository).bin.txt.ps1
@@ -1,7 +1,7 @@
 !#/usr/bin/env tea pwsh
 
 Set-Location
-Set-Variable -Name Username -Scope Global -Value rokejulianlockhart
+Set-Variable -Name Username -Scope Global -Value rokejulianlockhart # bw get username da37ced2-546e-4481-b9f7-accc01382414
 git clone --bare https://github.com/rokejulianlockhart/$(Get-Variable -Name 'Username' | Select-Object -ExpandProperty Value).git
 Set-Location "$(Get-Variable -Name 'Username' | Select-Object -ExpandProperty Value).git"
-git push --mirror "https://$(bw get username da37ced2-546e-4481-b9f7-accc01382414):$(bw get password da37ced2-546e-4481-b9f7-accc01382414)@github.com/rokejulianlockhart/$(Get-Variable -Name 'Username' | Select-Object -ExpandProperty Value).github.io.git"
+git push --mirror "https://$(Get-Variable -Name 'Username' | Select-Object -ExpandProperty Value):$((bw get item da37ced2-546e-4481-b9f7-accc01382414 | ConvertFrom-Json | Select-Object -ExpandProperty passwordHistory | Select-Object -ExpandProperty 'password')[2].Split(': ')[-1])@github.com/rokejulianlockhart/$(Get-Variable -Name 'Username' | Select-Object -ExpandProperty Value).github.io.git"
