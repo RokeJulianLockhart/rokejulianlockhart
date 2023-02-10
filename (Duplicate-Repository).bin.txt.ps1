8663ef69611869a9f0b01b1943a464e35b288b2f
beedell.rokejulianlockhart <rokejulianlockhart+1674177493@outlook.com>
2023-02-10 01:00:10 +0000

    Update (Duplicate-Repository).bin.txt.ps1
    Signed-off-by: beedell.rokejulianlockhart <rokejulianlockhart+1674177493@outlook.com>

--- (Duplicate-Repository).bin.txt.ps1
+++ (Duplicate-Repository).bin.txt.ps1
@@ -1,6 +1,7 @@
 !#/usr/bin/env tea pwsh
 
+
 Set-Variable -Name Username -Scope Global -Value rokejulianlockhart
-git clone --bare https://github.com/rokejulianlockhart/$(Get-Variable -Name 'Username' | Select-Object -ExpandProperty Value).git
+git clone --bare https://$(bw get username da37ced2-546e-4481-b9f7-accc01382414):$(bw get password da37ced2-546e-4481-b9f7-accc01382414)@github.com/rokejulianlockhart/$(Get-Variable -Name 'Username' | Select-Object -ExpandProperty Value).git
 Set-Location "$(Get-Variable -Name 'Username' | Select-Object -ExpandProperty Value).git"
 git push --mirror "https://github.com/rokejulianlockhart/$(Get-Variable -Name 'Username' | Select-Object -ExpandProperty Value).github.io.git"
